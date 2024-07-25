48b387db7d692e0b1ec48da21e1be17b1bcd44c9
Krishna-Nayak603 <krishna.nayak@godigit.com>
2024-07-25 18:23:01 +0530

    UC3: Use Console to edit person details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -24,5 +24,53 @@ public class AddressBookMain {
         String print = c.toString();
 
         System.out.println(print);
+
+        System.out.println("Enter a number to Edit the Number");
+        String personName = sc.nextLine();
+
+
+
+        System.out.println("Enter number of the field you want to change:\n" +
+                "1. first Name\n" +
+                "2. last Name\n" +
+                "3. address\n" +
+                "4. city\n" +
+                "5. state\n" +
+                "6. zip\n" +
+                "7. phone Number\n" +
+                "8. email");
+
+        String choice = sc.nextLine();
+
+        System.out.println("Enter the update text:");
+        String updateVal = sc.nextLine();
+
+        editContact(c,choice,updateVal);
+
+        print = c.toString();
+        System.out.println("Updated the user name: "+ personName);
+        System.out.println(print);
+
+
+
+    }
+
+//    public static Contact findByPersonName(String person){
+//
+//        return null;
+//    }
+
+    public static void editContact(Contact contact, String choice, String updateVal){
+        switch (choice){
+            case "1" -> contact.setFirstname(updateVal);
+            case "2" -> contact.setLastname(updateVal);
+            case "3" -> contact.setAddress(updateVal);
+            case "4" -> contact.setCity(updateVal);
+//            case "5" -> contact.setState(updateVal);
+            case "6" -> contact.setZip(updateVal);
+            case "7" -> contact.setPhoneNumber(updateVal);
+            case "8" -> contact.setEmail(updateVal);
+            default -> System.out.println("Invalid Input");
+        }
     }
 }
