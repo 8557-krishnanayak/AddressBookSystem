dbf6213bdda045d9037dc59114e0143e75973146
Krishna-Nayak603 <krishna.nayak@godigit.com>
2024-07-25 17:55:35 +0530

    UC2: added person details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstname = sc.nextLine();
+        System.out.println("Enter Last Name: ");
+        String lastname = sc.nextLine();
+        System.out.println("Enter Phone Number: ");
+        String phoneNumber = sc.nextLine();
+        System.out.println("Enter Address: ");
+        String address = sc.nextLine();
+        System.out.println("Enter Zip Name: ");
+        String zip = sc.nextLine();
+        System.out.println("Enter City Name: ");
+        String city = sc.nextLine();
+        System.out.println("Enter Email Name: ");
+        String email= sc.nextLine();
+
+        Contact c = new Contact(firstname,lastname,phoneNumber,address,zip,city,email);
+        String print = c.toString();
+
+        System.out.println(print);
     }
 }
